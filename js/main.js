914c157d50dda93966f0528aab29a631449f478d
mzsellem-GA <125702378+mzsellem-GA@users.noreply.github.com>
2023-04-24 15:16:32 -0400

    childDiv creating loop and format

--- js/main.js
+++ js/main.js
@@ -1,35 +1,56 @@
  /*----- constants -----*/
 // flagsArray1 = [
-//     {front: 'australian-flag.png', back: 'fields-back-of-card.png', isVisible = false, isMatch = false},
-//     {front: 'brazilian-flag.png', back: 'fields-back-of-card.png', isVisible = false, isMatch = false},
-//     {front: 'canadian-flag.png', back: 'fields-back-of-card.png', isVisible = false, isMatch = false},
-//     {front: 'french-flag.png', back: 'fields-back-of-card.png', isVisible = false, isMatch = false},
-//     {front: 'indian-flag.png', back: 'fields-back-of-card.png', isVisible = false, isMatch = false},
-//     {front: 'romanian-flag.png', back: 'fields-back-of-card.png', isVisible = false, isMatch = false},
-//     {front: 'usa-flag.png', back: 'fields-back-of-card.png', isVisible = false, isMatch = false},
-//     {front: 'japanese-flag.png', back: 'fields-back-of-card.png', isVisible = false, isMatch = false}
+//     {front: 'australian-flag.png', back: 'fields-back-of-card.png'},
+//     {front: 'brazilian-flag.png', back: 'fields-back-of-card.png'},
+//     {front: 'canadian-flag.png', back: 'fields-back-of-card.png'},
+//     {front: 'french-flag.png', back: 'fields-back-of-card.png'},
+//     {front: 'indian-flag.png', back: 'fields-back-of-card.png'},
+//     {front: 'romanian-flag.png', back: 'fields-back-of-card.png'},
+//     {front: 'usa-flag.png', back: 'fields-back-of-card.png'},
+//     {front: 'japanese-flag.png', back: 'fields-back-of-card.png'}
 // ];
 // flagsArray2 = [
-//     {front: 'australian-flag.png', back: 'fields-back-of-card.png', isMatch = false},
-//     {front: 'brazilian-flag.png', back: 'fields-back-of-card.png', isMatch = false},
-//     {front: 'canadian-flag.png', back: 'fields-back-of-card.png', isMatch = false},
-//     {front: 'french-flag.png', back: 'fields-back-of-card.png', isMatch = false},
-//     {front: 'indian-flag.png', back: 'fields-back-of-card.png', isMatch = false},
-//     {front: 'romanian-flag.png', back: 'fields-back-of-card.png', isMatch = false},
-//     {front: 'usa-flag.png', back: 'fields-back-of-card.png', isMatch = false},
-//     {front: 'japanese-flag.png', back: 'fields-back-of-card.png', isMatch = false},
+//     {front: 'australian-flag.png', back: 'fields-back-of-card.png'},
+//     {front: 'brazilian-flag.png', back: 'fields-back-of-card.png'},
+//     {front: 'canadian-flag.png', back: 'fields-back-of-card.png'},
+//     {front: 'french-flag.png', back: 'fields-back-of-card.png'},
+//     {front: 'indian-flag.png', back: 'fields-back-of-card.png'},
+//     {front: 'romanian-flag.png', back: 'fields-back-of-card.png'},
+//     {front: 'usa-flag.png', back: 'fields-back-of-card.png'},
+//     {front: 'japanese-flag.png', back: 'fields-back-of-card.png'},
 // ]
 
   /*----- state variables -----*/
-let timer;
-let isSolved;
+// let timer;
+// let isSolved;
 
   /*----- cached elements  -----*/
-// let parentDiv = document.createElement("div").classList.add("parent-div");
-// document.body.append(parentDiv);
-// let childDiv= parentDiv.createElement("div").classList.add("child");
-// parentDiv.append(childDiv);
+const parentDiv = document.querySelector(".parent-div");
 
+function createChildDivs (num) {
+    for(let i=0; i < num; i++){
+        let childDiv = document.createElement("div");
+        childDiv.classList.add("child-div");
+        childDiv.style.height = "150px"
+        childDiv.style.width = "200px"
+        parentDiv.append(childDiv);
+        }
+}
+createChildDivs(16);
+
+
+
+
+// let childDiv = document.createElement("div");
+// childDiv.classList.add("child");
+// parentDiv.append(childDiv);
+// console.log(parentDiv);
+
+
+
+// const imageInChildDiv = parentDiv.createElement("img");
+// img.src = "australian-flag.png";
+// childDiv.appendChild(imageInChildDiv);
 
 
   /*----- event listeners -----*/
