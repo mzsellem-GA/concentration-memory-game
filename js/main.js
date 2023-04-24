3e04fd1f376ce6183a13ba5e31a5e8a4e00aad8e
mzsellem-GA <125702378+mzsellem-GA@users.noreply.github.com>
2023-04-24 16:43:32 -0400

    board design and alignment

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
  /*----- constants -----*/
-// flagsArray1 = [
-//     {front: 'australian-flag.png', back: 'fields-back-of-card.png'},
-//     {front: 'brazilian-flag.png', back: 'fields-back-of-card.png'},
-//     {front: 'canadian-flag.png', back: 'fields-back-of-card.png'},
-//     {front: 'french-flag.png', back: 'fields-back-of-card.png'},
-//     {front: 'indian-flag.png', back: 'fields-back-of-card.png'},
-//     {front: 'romanian-flag.png', back: 'fields-back-of-card.png'},
-//     {front: 'usa-flag.png', back: 'fields-back-of-card.png'},
-//     {front: 'japanese-flag.png', back: 'fields-back-of-card.png'}
-// ];
+flagsArray1 = [
+    {front: 'australian-flag.png', back: 'fields-back-of-card.png'},
+    {front: 'brazilian-flag.png', back: 'fields-back-of-card.png'},
+    {front: 'canadian-flag.png', back: 'fields-back-of-card.png'},
+    {front: 'french-flag.png', back: 'fields-back-of-card.png'},
+    {front: 'indian-flag.png', back: 'fields-back-of-card.png'},
+    {front: 'romanian-flag.png', back: 'fields-back-of-card.png'},
+    {front: 'usa-flag.png', back: 'fields-back-of-card.png'},
+    {front: 'japanese-flag.png', back: 'fields-back-of-card.png'}
+];
 // flagsArray2 = [
 //     {front: 'australian-flag.png', back: 'fields-back-of-card.png'},
 //     {front: 'brazilian-flag.png', back: 'fields-back-of-card.png'},
@@ -26,7 +26,6 @@
 
   /*----- cached elements  -----*/
 const parentDiv = document.querySelector(".parent-div");
-
 function createChildDivs (num) {
     for(let i=0; i < num; i++){
         let childDiv = document.createElement("div");
@@ -40,20 +39,42 @@ createChildDivs(16);
 
 
 
+function insertFlag () {
+    let childDiv = document.querySelector(".child-div");
+    const flagTag = document.createElement("img");
+    img.src = "https://www.rawpixel.com/board/537381/vincent-van-gogh-free-original-cc0-public-domain-paintings";
+    childDiv.appendChild(flagTag);
+}
+insertFlag();
 
-// let childDiv = document.createElement("div");
-// childDiv.classList.add("child");
-// parentDiv.append(childDiv);
-// console.log(parentDiv);
 
 
 
-// const imageInChildDiv = parentDiv.createElement("img");
-// img.src = "australian-flag.png";
-// childDiv.appendChild(imageInChildDiv);
 
 
   /*----- event listeners -----*/
 
 
   /*----- functions -----*/
+  let timer; 
+  let timeLeft = 60;
+  
+  function gameOver() {
+    cancelInterval(timer);
+    $('#playAgainButton').show();
+  }
+  
+  function updateTimer() {
+    timeLeft = timeLeft - 1;
+    if(timeLeft >= 0)
+      $('#timer').html(timeLeft);
+    else {
+      gameOver();
+    }
+  }
+  
+  function start() {
+    timer = setInterval(updateTimer, 1000);
+    updateTimer();
+     $('#playAgainButton').hide();
+  }
